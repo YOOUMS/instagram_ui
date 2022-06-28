7eea716db23c23d8041d6638507a466fd4e46d7e
youms <yussef.msabeh@gmail.com>
2022-06-28 09:45:13 +0300

    upload instagram ui

---
+++ android/app/src/main/kotlin/com/example/instagrem_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.instagrem_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:instagrem_ui/instgramUI.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: instgramUI(),
+  ));
+}
